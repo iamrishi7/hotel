e8ee32cdad365e3e287625057b0ccac3142b45e8
sangam-cm <sangam@cloudmagician.in>
2023-06-23 16:24:39 +0530

    hero

--- pages/index.vue
+++ pages/index.vue
@@ -3,5 +3,11 @@ const route = useRoute()
 </script>
 
 <template>
-<div class="w-[100%] m-6 h-[36rem] bg-green-200"></div>
+    <div class="w-[100%] place-items-center">
+        <div class="relative w-[98%] m-6 h-[36rem] bg-gradient-to-r from-blue-200  rounded-md">
+            <img class="h-[36rem] w-[100%] object-cover mix-blend-overlay rounded-md" src="~/assets/images/hero.jpg" alt="">
+        </div>
+        <div class="mx-[24rem] absolute h-[18rem] bg-blue-200 rounded-xl">
+        </div>
+    </div>
 </template>
